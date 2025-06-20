6e75f6a4bfdcef1a25886f2b5eb67ed0f352b847
krv006 <rvkamronbek@gmail.com>
2025-06-20 12:13:13 +0500

    model

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
-from django.db.models import CharField, EmailField, BooleanField, Model, DateTimeField, DateField
+from django.core.exceptions import ValidationError
+from django.db.models import CharField, EmailField, BooleanField, Model, DateTimeField, CASCADE, ForeignKey
 from django.utils import timezone
 
 from users.managers import CustomUserManager
@@ -30,3 +31,22 @@ class User(AbstractUser):
 
     def full_name(self):
         return f'{self.first_name} {self.last_name}'
+
+
+class Manager(Model):
+    user = ForeignKey('users.User', CASCADE, related_name='manager_profile')
+
+    # todo shared model yozish kere
+    # created_at = models.DateTimeField(auto_now_add=True)
+    # updated_at = models.DateTimeField(auto_now=True)
+
+    def clean(self):
+        if self.user.role != 'manager':
+            raise ValidationError("Faqatgina 'manager' roliga ega foydalanuvchi Manager bo'lishi mumkin.")
+
+    def save(self, *args, **kwargs):
+        self.clean()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"Manager: {self.user.full_name()} ({self.user.email})"
