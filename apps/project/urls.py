caa69fab139fa83edf34f9eafe5f97a1033bcfb8
krv006 <rvkamronbek@gmail.com>
2025-06-26 18:47:10 +0500

    project api view

--- apps/project/urls.py
+++ apps/project/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 
+from project.views import ProjectListCreateAPIView, ProjectUserListCreateAPIView
+
 urlpatterns = [
+    # todo Project API
+    path('project-list/', ProjectListCreateAPIView.as_view(), name='project-list'),
+    path('project-user/', ProjectUserListCreateAPIView.as_view(), name='project-user'),
 
 ]
--- apps/project/views.py
+++ apps/project/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from drf_spectacular.utils import extend_schema
+from rest_framework.generics import ListCreateAPIView
 
-# Create your views here.
+from project.models import Project, ProjectUser
+from project.serializers import ProjectModelSerializer, ProjectUserModelSerializer
+
+
+@extend_schema(tags=['Project Api '], description="""
+Project create 
+""")
+class ProjectListCreateAPIView(ListCreateAPIView):
+    queryset = Project.objects.all()
+    serializer_class = ProjectModelSerializer
+    # permission_classes = AllowAny,
+
+
+@extend_schema(tags=['Project Api '], description="""
+Project create 
+""")
+class ProjectUserListCreateAPIView(ListCreateAPIView):
+    queryset = ProjectUser.objects.all()
+    serializer_class = ProjectUserModelSerializer
+    # permission_classes = AllowAny,
