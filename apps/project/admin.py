e54568f44310f47b72f9dfcd76b58bebf60422fa
krv006 <rvkamronbek@gmail.com>
2025-06-26 11:35:13 +0500

    admin

--- apps/project/admin.py
+++ apps/project/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
 
-# Register your models here.
+from project.models import Project, ProjectUser
+
+
+@admin.register(Project)
+class ProjectAdmin(ModelAdmin):
+    pass
+
+
+@admin.register(ProjectUser)
+class ProjectUserAdmin(ModelAdmin):
+    pass
