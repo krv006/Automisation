74e12b144d01e1f4c96e778ddc63746c3afe57db
krv006 <rvkamronbek@gmail.com>
2025-06-27 13:32:50 +0500

    Project create done

--- apps/project/serializers.py
+++ apps/project/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework.serializers import ModelSerializer
 
 from project.models import Project, ProjectUser
+from users.serializers import UserDetailModelSerializer
 
 
 class ProjectModelSerializer(ModelSerializer):
@@ -8,9 +9,14 @@ class ProjectModelSerializer(ModelSerializer):
         model = Project
         fields = 'id', 'name', 'description', 'created_by', 'manager',
 
+    def to_representation(self, instance):
+        repr = super().to_representation(instance)
+        repr['created_by'] = UserDetailModelSerializer(instance.created_by).data
+        repr['manager'] = UserDetailModelSerializer(instance.manager).data
+        return repr
+
 
 class ProjectUserModelSerializer(ModelSerializer):
     class Meta:
         model = ProjectUser
         fields = 'id', 'project', 'assigned_by',
-
